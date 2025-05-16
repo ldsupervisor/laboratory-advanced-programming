18abb2ab6e0a9d0592989de6c5632ca67170b286
Lukasz Dyga <lukasz.dyga@chorzow.merito.pl>
2025-05-16 16:38:53 +0200

    lab 2 update

---
+++ Lab2/Solutions/Exercise1/Infrastructure/Packages/LargePackageType/LargePackageFactory.cs
@@ -0,0 +1,14 @@
+﻿using Exercise1.Infrastructure.Factories;
+using FactoryMethodLogistics.Core.Interfaces;
+using Logistics.Core.Interfaces;
+
+namespace Logistics.Infrastructure.Factories
+{
+    public class LargePackageFactory : IPackageFactory
+    {
+        public IPackage InstantiatePackage()
+        {
+            return new LargePackage();
+        }
+    }
+}
