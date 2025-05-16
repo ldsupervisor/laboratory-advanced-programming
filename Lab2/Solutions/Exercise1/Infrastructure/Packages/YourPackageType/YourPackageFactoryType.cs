78b78843f0ef670f0af97aab4ef117108d420183
Lukasz Dyga <lukasz.dyga@chorzow.merito.pl>
2025-05-16 16:57:25 +0200

    lab updates

---
+++ Lab2/Solutions/Exercise1/Infrastructure/Packages/YourPackageType/YourPackageFactoryType.cs
@@ -0,0 +1,14 @@
+﻿using Exercise1.Infrastructure.Factories;
+using FactoryMethodLogistics.Core.Interfaces;
+using Logistics.Core.Interfaces;
+
+namespace Logistics.Infrastructure.Factories
+{
+    public class YourPackageFactoryType : IPackageFactory
+    {
+        public IPackage InstantiatePackage()
+        {
+            return new SmallPackage();
+        }
+    }
+}
---
+++ Lab2/Solutions/Exercise1/Infrastructure/Packages/YourPackageType/YourPackageType.cs
@@ -0,0 +1,21 @@
+﻿using Logistics.Core.Interfaces;
+
+namespace Exercise1.Infrastructure.Factories
+{
+    public class SmallPackage : IPackage
+    {
+
+        private static int _globalCounter = 0;
+        private readonly int _id;
+
+
+        public SmallPackage()
+        {
+            _id = ++_globalCounter;
+        }
+        public void PreparePackage()
+        {
+            Console.WriteLine($"Small package number {_id} prepared.");
+        }
+    }
+}
