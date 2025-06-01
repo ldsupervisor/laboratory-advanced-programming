2d82b0fb486fa02dd0e0d3f1a9043d0237a79878
Lukasz Dyga <lukasz.dyga@chorzow.merito.pl>
2025-06-01 13:33:12 +0200

    update patterns examples

--- VisitorPattern/Program.cs
+++ VisitorPattern/Program.cs
@@ -8,13 +8,13 @@ IGuest sibling = new Sibling();
 // The level of cleanliness
 Console.WriteLine($"Initial cleanliness: {house.Cleanliness}\n");
 
-// Calling visiting
+// Calling visiting objects, it could be expanded
 house.Accept(sibling);
 house.Accept(mother);
-house.Accept(sibling);
 
-// Definitions
-
+// Definitions of guest visitor interface
+// Contract for visitors
+// Can be easily expanded without modyfiyng the house
 public interface IGuest
 {
     void Visit(House house);
@@ -34,7 +34,7 @@ public class Mother : IGuest
 {
     public void Visit(House house)
     {
-        house.Cleanliness += 3;
+        house.Cleanliness += 50;
         Console.WriteLine($"Mother visit. Cleanliness now: {house.Cleanliness}.");
     }
 }
@@ -43,7 +43,7 @@ public class Sibling : IGuest
 {
     public void Visit(House house)
     {
-        house.Cleanliness += 1;
+        house.Cleanliness += -8;
         Console.WriteLine($"Sibling visit. Cleanliness now: {house.Cleanliness}.");
     }
 }
